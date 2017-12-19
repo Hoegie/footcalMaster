979f6ffd527e0fadbc13f84cd7169b397b629b4f
Hoegie <mezenhof8@gmail.com>
2017-12-19 19:08:54 +0100

    2nd commit

---
+++ footcalhook.sh
@@ -0,0 +1,4 @@
+date >> /app/nodeprojects/github/logs/footcalhook.log 2>&1
+cd /app/nodeprojects/github/footcalMaster
+git pull https://github.com/Hoegie/footcalMaster.git >> /app/nodeprojects/github/logs/footcalhook.log 2>&1
+
