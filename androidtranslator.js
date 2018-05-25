07862d6e68622236631116b1e5edca40a186ae19
Hoegie <mezenhof8@gmail.com>
2018-05-25 15:43:37 +0200

    nth commit

---
+++ androidtranslator.js
@@ -0,0 +1,22 @@
+var translator = {
+
+	"en": {
+
+			"game_start": "The %1 match has started"
+	},
+
+	"nl": {
+
+			"game_start": "De %1 wedstrijd is gestart"
+	},
+
+	"fr": {
+
+			"game_start": "Le match %1 a commencé"
+	}
+
+
+
+};
+
+module.exports.translator = translator;
