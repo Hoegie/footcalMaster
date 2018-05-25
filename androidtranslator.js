a430c182273286f08b7b46860383f31bf8e3304e
Hoegie <mezenhof8@gmail.com>
2018-05-25 16:10:22 +0200

    nth commit

--- androidtranslator.js
+++ androidtranslator.js
@@ -2,17 +2,59 @@ var translator = {
 
 	"en": {
 
-			"game_start": "The %1 match has started"
+			"game_start": "The %1 match has started",
+			"game_end": "The %1 match has ended",
+			"goal_scored": "Goal !! (%1 game)",
+			"player_scored": "%1 has scored : %2 - %3",
+			"annulation": "Cancelled !",
+			"Training": "%1 %2 Training",
+			"Comp. Match": "%1 %2 Comp. Game",
+			"Vriend. Match": "%1 %2 Friendly Game",
+			"Vacla": "%1 %2 Vacla",
+			"Tornooi": "%1 %2 Tournament",
+			"game_moved": "The game of %1 is moved to %2",
+			"vacla_moved": "The Vacla game of %1 is moved to %2",
+			"tournament_moved": "The tournament of %1 is moved to %2",
+			"training_moved": "The training of %1 is moved to %2",
+			"event_moved": "Event move !"
 	},
 
 	"nl": {
 
-			"game_start": "De %1 wedstrijd is gestart"
+			"game_start": "De %1 wedstrijd is gestart",
+			"game_end": "De %1 wedstrijd is afgelopen",
+			"goal_scored": "Goal !! (%1 wedstrijd)",
+			"player_scored": "%1 heeft gescoord : %2 - %3",
+			"annulation": "Afgelasting !",
+			"Training": "%1 %2 Training",
+			"Comp. Match": "%1 %2 Comp. Match",
+			"Vriend. Match": "%1 %2 Vriend. Match",
+			"Vacla": "%1 %2 Vacla",
+			"Tornooi": "%1 %2 Tornooi",
+			"game_moved": "De wedstrijd van %1 wordt verplaatst naar %2",
+			"vacla_moved": "De Vacla wedstrijd van %1 wordt verplaatst naar %2",
+			"tournament_moved": "Het tornooi van %1 wordt verplaatst naar %2",
+			"training_moved": "De training van %1 wordt verplaatst naar %2",
+			"event_moved": "Verplaatsing !"
 	},
 
 	"fr": {
 
-			"game_start": "Le match %1 a commencé"
+			"game_start": "Le match %1 a commencé",
+			"game_end": "Le match %1 est terminé",
+			"goal_scored": "But !! (match %1)",
+			"player_scored": "%1 a marqué : %2 - %3",
+			"annulation": "Annulé !",
+			"Training": "%1 %2 Entraînement",
+			"Comp. Match": "%1 %2 Jeu Comp.",
+			"Vriend. Match": "%1 %2 Jeu Amical",
+			"Vacla": "%1 %2 Vacla",
+			"Tornooi": "%1 %2 Tournoi",
+			"game_moved": "Le match de %1 sera déplacé à %2",
+			"vacla_moved": "Le match Vacla de %1 sera déplacé au %2",
+			"tournament_moved": "Le tournoi de %1 sera déplacé au %2",
+			"training_moved": "l'Entraînement de %1 sera déplacé au %2",
+			"event_moved": "Déplacement !"
 	}
 
 
