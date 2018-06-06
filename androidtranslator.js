08934c13e31b9d5795d8ab2f69336dc010cccbf2
Hoegie <mezenhof8@gmail.com>
2018-06-06 09:00:17 +0200

    nth commit

--- androidtranslator.js
+++ androidtranslator.js
@@ -4,9 +4,13 @@ var translator = {
 
 			"game_start": "The %1 match has started",
 			"game_end": "The %1 match has ended",
+			"club_game_start": "%2 The %1 match has started",
+			"club_game_end": "%2 The %1 match has ended",
 			"goal_scored": "Goal !! (%1 game)",
+			"club_goal_scored": "%2 Goal !! (%1 game)",
 			"player_scored": "%1 has scored : %2 - %3",
 			"annulation": "Cancelled !",
+			"club_annulation": "%1 Cancelled !",
 			"Training": "%1 %2 Training",
 			"Comp. Match": "%1 %2 Comp. Game",
 			"Vriend. Match": "%1 %2 Friendly Game",
@@ -16,16 +20,21 @@ var translator = {
 			"vacla_moved": "The Vacla game of %1 is moved to %2",
 			"tournament_moved": "The tournament of %1 is moved to %2",
 			"training_moved": "The training of %1 is moved to %2",
-			"event_moved": "Event move !"
+			"event_moved": "Event move !",
+			"club_event_moved": "%1 Event move !"
 	},
 
 	"nl": {
 
 			"game_start": "De %1 wedstrijd is gestart",
 			"game_end": "De %1 wedstrijd is afgelopen",
+			"club_game_start": "%2 De %1 wedstrijd is gestart",
+			"club_game_end": "%2 De %1 wedstrijd is afgelopen",
 			"goal_scored": "Goal !! (%1 wedstrijd)",
+			"club_goal_scored": "%2 Goal !! (%1 wedstrijd)",
 			"player_scored": "%1 heeft gescoord : %2 - %3",
 			"annulation": "Afgelasting !",
+			"club_annulation": "%1 Afgelasting !",
 			"Training": "%1 %2 Training",
 			"Comp. Match": "%1 %2 Comp. Match",
 			"Vriend. Match": "%1 %2 Vriend. Match",
@@ -35,16 +44,21 @@ var translator = {
 			"vacla_moved": "De Vacla wedstrijd van %1 wordt verplaatst naar %2",
 			"tournament_moved": "Het tornooi van %1 wordt verplaatst naar %2",
 			"training_moved": "De training van %1 wordt verplaatst naar %2",
-			"event_moved": "Verplaatsing !"
+			"event_moved": "Verplaatsing !",
+			"club_event_moved": "%1 Verplaatsing !"
 	},
 
 	"fr": {
 
 			"game_start": "Le match %1 a commencé",
 			"game_end": "Le match %1 est terminé",
+			"club_game_start": "%2 Le match %1 a commencé",
+			"club_game_end": "%2 Le match %1 est terminé",
 			"goal_scored": "But !! (match %1)",
+			"club_goal_scored": "%2 But !! (match %1)",
 			"player_scored": "%1 a marqué : %2 - %3",
 			"annulation": "Annulé !",
+			"club_annulation": "%1 Annulé !",
 			"Training": "%1 %2 Entraînement",
 			"Comp. Match": "%1 %2 Jeu Comp.",
 			"Vriend. Match": "%1 %2 Jeu Amical",
@@ -54,7 +68,8 @@ var translator = {
 			"vacla_moved": "Le match Vacla de %1 sera déplacé au %2",
 			"tournament_moved": "Le tournoi de %1 sera déplacé au %2",
 			"training_moved": "l'Entraînement de %1 sera déplacé au %2",
-			"event_moved": "Déplacement !"
+			"event_moved": "Déplacement !",
+			"club_event_moved": "%1 Déplacement !"
 	}
 
 
