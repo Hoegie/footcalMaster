319feab76cf05c23736594cc864d9f43ce4fe02f
Hoegie <mezenhof8@gmail.com>
2019-03-22 07:26:27 +0100

    nth commit

--- createdb.sh
+++ createdb.sh
@@ -1,7 +1,7 @@
 dbname=FootCal_$1
 createcommand="\"CREATE DATABASE $dbname;\""
 date >> /app/nodeprojects/github/logs/newClubAccount.log 2>&1
-#mysql -uroot -pHoegaarden -e $createcommand >> /app/nodeprojects/github/logs/newClubAccount.log 2>&1  
-echo $createcommand >> /app/nodeprojects/github/logs/newClubAccount.log 2>&1  
+echo $createcommand >> /app/nodeprojects/github/logs/newClubAccount.log 2>&1 
+mysql -uroot -pHoegaarden -e $createcommand >> /app/nodeprojects/github/logs/newClubAccount.log 2>&1
 
 
